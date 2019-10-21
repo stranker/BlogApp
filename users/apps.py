0876e7215e8505746895f1551230d600c12a6ac9
Unknown <dani_mrx93@hotmail.com>
2019-10-21 05:15:16 -0300

    Add signals to create Profile when User is created
    Also add default.jpg in order to show default image on Profile created without image

--- users/apps.py
+++ users/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+    #Se importa aca adentro para hacer un safe import y que no ocurran problemas
+    def ready(self):
+        import users.signals
