4998ce35a909e5f59157239e7df441734b6ee7dc
Unknown <dani_mrx93@hotmail.com>
2019-10-22 04:55:24 -0300

    Remove unused imports
    Remove imports on Users app and Blog App

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
-from blog.models import Comment
 
 
 class UserRegisterForm(UserCreationForm):
